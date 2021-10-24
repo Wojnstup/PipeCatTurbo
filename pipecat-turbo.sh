b41ba460ca2c20f13a5a14b7dfe8bac1b556a585
Wojnstup <78437858+Wojnstup@users.noreply.github.com>
2021-10-24 18:38:30 +0000

    Add files via upload

--- pipecat-turbo.sh
+++ pipecat-turbo.sh
@@ -2,7 +2,7 @@
 
 ## To use this you will need socat installed
 socket_path="/tmp/mpvsocket"
-
+list_file="$HOME/.pipecat_turbo_lists"
 
 ## This is major! These URLS redirect to a privacy-respecting YouTube fronted, so you don't even ping google!
 url="https://iteroni.com/search?q="
@@ -34,6 +34,7 @@ get_playlist_content(){
 	html=$( curl -s "$video_url""$1" )
 
 	titles=$( echo "$html" | grep '<p dir="auto">' | awk -F'<p dir="auto">' '{ print $2 }' | awk -F'</p>' '{ print $1 }' | grep -n "" )
+	echo "$titles"
 	urls=$( echo "$html" | grep '<a style="width:100%" href=' | awk -F\" '{ print $4 }' )
 }
 
@@ -48,11 +49,11 @@ search_channel(){
 while :
 do
 
-	search_option=$( echo -e "Controlls\nVideo\nPlaylist\nChannel\nAudio mode" | dmenu -sb '#98005d' -fn "Terminus:bold:size:15" -h 27 -p "Search option: " )
+	search_option=$( echo -e "Controlls\nVideo\nPlaylist\nChannel\nYour Lists\nAudio mode" | dmenu -sb '#98005d' -fn "Terminus:bold:size:15" -h 27 -p "Search option: " )
 	
 	if [[ $search_option == "Audio mode" ]]	
 	then
-		search_option=$( echo -e "Controlls\nVideo\nPlaylist\nChannel\nAudio mode" | dmenu -sb '#98005d' -fn "Terminus:bold:size:15" -h 27 -p "Audio mode set: " )
+		search_option=$( echo -e "Controlls\nVideo\nPlaylist\nChannel\nYour Lists\nAudio mode" | dmenu -sb '#98005d' -fn "Terminus:bold:size:15" -h 27 -p "Audio mode set: " )
 		audio_mode="--no-video"
 	fi
 
@@ -82,7 +83,7 @@ do
 		echo quit | socat - /tmp/mpvsocket	
 		notify-send "Playing '$choice'"
 		echo "$choice" > /tmp/currently_playlist_pipecat
-		choice=$( echo "$titles" | grep -n "$choice" | awk -F: '{ print $1 }' )
+		choice=$( echo "$choice" | awk -F: '{ print $1 }' )
 
 		## Get the url with index $choice and play it in mpv
 		mpv $audio_mode  --input-ipc-server=$socket_path "$video_url$( echo "$urls" | sed -n $choice\p )"
@@ -111,7 +112,7 @@ do
 		notify-send "Playing '$choice'"
 		echo "$choice" > /tmp/currently_playlist_pipecat		
 
-		choice=$( echo "$titles" | grep -n "$choice" | awk -F: '{ print $1 }' ) 
+		choice=$( echo "$choice" | awk -F: '{ print $1 }' ) 
 		## Get the url with index $choice and play it in mpv
 		
 		mpv $audio_mode --input-ipc-server=$socket_path "$video_url$( echo "$urls" | sed -n $choice\p )"
@@ -132,7 +133,7 @@ do
 
 		get_playlist_content "$choice" 
 		choice=$( echo "$titles" | dmenu -sb '#98005d' -fn "Terminus:bold:size:15" -h 27 -l 10 )
-
+		echo $choice
 		if [[ $choice == "" ]]
 		then
 			break
@@ -142,7 +143,9 @@ do
 		notify-send "Playing '$choice'"
 		echo "$choice" > /tmp/currently_playlist_pipecat
 
-		choice=$( echo "$titles" | grep -n "$choice" | awk -F: '{ print $1 }' )
+		choice=$( echo "$choice" | awk -F: '{ print $1 }' )
+		
+		echo "$choice  <---------"
 		## Get the url with index $choice and play it in mpv
 		mpv $audio_mode --input-ipc-server=$socket_path "$video_url$( echo "$urls" | sed -n $choice\p )"
 	elif [[ $search_option == "Controlls" ]]
@@ -165,6 +168,35 @@ do
 				echo quit | socat - /tmp/mpvsocket
 				;;
 		esac
+	elif [[ $search_option == "Your Lists" ]]
+	then
+		## Make sure list file exists
+		touch $list_file
+
+		## Get contents of list file
+		file=$( cat $list_file )
+
+		## Extract list names and prompt user to choose a list
+		list=$(cat "$HOME""/.pipecat_turbo_lists" | grep "####- START LIST" | awk -F\< '{ print $2 }' | awk -F\> '{ print $1 }' | dmenu -sb '#98005d' -fn "Terminus:bold:size:15" -h 27 -l 10 )
+		
+		## Get the starting index and the file lenght of playlist file to get playlist starting and ending index in the next for loop. This should be done better than I did it but I'm so tired I just can't do it
+		start_index=$( cat "$HOME""/.pipecat_turbo_lists" | grep -n "####- START LIST <$list>" | awk -F\: '{ print $1 }' )
+		file_lenght=$( cat  "$HOME""/.pipecat_turbo_lists" | wc -l )
+		
+		for index in $( seq $file_lenght)
+		do
+			line=$( echo "$file" | sed -n $index\p )
+			if (( $index > $start_index )) && [ "$line" = "####- END LIST -####" ]
+			then
+				break
+			fi
+		done
+		
+		## Extracting the urls between start_index and index - it should be named end_index or something but i juwst don't care anymore. This took 3 hours to implement. Never code while tired.
+		content=$(echo "$file" | sed -n "$((start_index + 1)),$((index - 1))""p" )
+		echo "$content" | awk '{ print $NF }' > /tmp/pipecat_list
+
+		mpv $audio_mode --input-ipc-server=$socket_path --playlist=/tmp/pipecat_list
 	fi
 	break
 done
