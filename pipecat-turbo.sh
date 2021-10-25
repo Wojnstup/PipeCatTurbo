0bc06041d55e2f3a32dc74cdb85cade073b464f4
Wojnstup <78437858+Wojnstup@users.noreply.github.com>
2021-10-25 16:50:41 +0000

    Pain. Add to playlists.
    This is the most buggy project of mine right now. Added an option to add new urls to lists and stuff and improved youtube playlists playback too, now the media controls work there.

--- pipecat-turbo.sh
+++ pipecat-turbo.sh
@@ -4,10 +4,13 @@
 socket_path="/tmp/mpvsocket"
 list_file="$HOME/.pipecat_turbo_lists"
 
-## This is major! These URLS redirect to a privacy-respecting YouTube fronted, so you don't even ping google!
-url="https://iteroni.com/search?q="
-video_url="https://iteroni.com"
-
+## This is major! These URLS redirect to a privacy-respecting YouTube fronted, so you don't even ping google! If iteroni is down, try yewtu.be, if yewtu.be is down, try iteroni
+## This uses invidious, that is self hosted. You can even host it on your own and use this script that way. Just replace these urls
+#url="https://iteroni.com/search?q="
+#video_url="https://iteroni.com"
+url="https://yewtu.be/search?q="
+video_url="https://yewtu.be"
+sed_url="https\:\/\/yewtu.be"
 IFS=$'\n'
 
 ###### Functions ######
@@ -34,8 +37,9 @@ get_playlist_content(){
 	html=$( curl -s "$video_url""$1" )
 
 	titles=$( echo "$html" | grep '<p dir="auto">' | awk -F'<p dir="auto">' '{ print $2 }' | awk -F'</p>' '{ print $1 }' | grep -n "" )
-	echo "$titles"
 	urls=$( echo "$html" | grep '<a style="width:100%" href=' | awk -F\" '{ print $4 }' )
+
+	echo "URLS""$urls"
 }
 
 ## Sets titles and urls found channels
@@ -46,15 +50,49 @@ search_channel(){
 	urls=$( echo "$html" | grep '<a href="/channel' | awk -F\" '{ print $2 }' | awk -F\/ '{ print $3 }' )
 }
 
+add_to_list(){
+	file=$( cat $list_file )
+	start_index=$( cat "$HOME""/.pipecat_turbo_lists" | grep -n "####- START LIST <$1>" | awk -F\: '{ print $1 }' )
+	file_lenght=$( cat  "$HOME""/.pipecat_turbo_lists" | wc -l )
+	if [[ -z $start_index ]]
+		then
+		echo "doesn't exist"
+		break
+	fi
+
+	for index in $( seq $file_lenght)
+	do
+		line=$( echo "$file" | sed -n $index\p )
+		if (( $index > $start_index )) && [ "$line" = "####- END LIST -####" ]
+		then
+			break
+		fi
+	done
+	
+	echo "$( cat "$HOME""/.pipecat_turbo_lists" | sed "$index"'i'"$2" )" > $list_file
+}
+
+
+
+### MAIN ###
 while :
 do
-
-	search_option=$( echo -e "Controlls\nVideo\nPlaylist\nChannel\nYour Lists\nAudio mode" | dmenu -sb '#98005d' -fn "Terminus:bold:size:15" -h 27 -p "Search option: " )
+	## First menu that pops up
+	search_option=$( echo -e "Controlls\nVideo\nPlaylist\nChannel\nYour Lists\nAudio mode\nShuffle mode\nAdd to list" | dmenu -sb '#98005d' -fn "Terminus:bold:size:15" -h 27 -p "Search option: " )
 	
+	## If you selected Audio Mode, relaunch the menu in audio mode
 	if [[ $search_option == "Audio mode" ]]	
 	then
 		search_option=$( echo -e "Controlls\nVideo\nPlaylist\nChannel\nYour Lists\nAudio mode" | dmenu -sb '#98005d' -fn "Terminus:bold:size:15" -h 27 -p "Audio mode set: " )
 		audio_mode="--no-video"
+	fi
+	
+	## If you selected add to list, relaunch the menu in add to list mode
+	if [[ $search_option == "Add to list" ]]
+	then
+		list=$(cat "$HOME""/.pipecat_turbo_lists" | grep "####- START LIST" | awk -F\< '{ print $2 }' | awk -F\> '{ print $1 }' | dmenu -sb '#98005d' -fn "Terminus:bold:size:15" -h 27 -l 10 )
+		search_option=$( echo -e "Video\nPlaylist\nChannel" | dmenu -sb '#98005d' -fn "Terminus:bold:size:15" -h 27 -p "Find what you want to add: " )
+		add_to_list="True"
 	fi
 
 	if [[ $search_option == "Video" ]]
@@ -75,14 +113,30 @@ do
 		## Let user select a title, then get index of that title
 		choice=$( echo "$titles" | dmenu -sb '#98005d' -fn "Terminus:bold:size:15" -h 27 -l 10 )
 
-		if (( ${#choice}==0 ))
+		if [[ -z $choice ]]
 		then
 			break
 		fi
 		
+		## EXTREMELY IMPORTANT! When option set to add to list, add to list and break out of the loop	
+		if [[ -v add_to_list ]]
+		then
+			title="$choice"
+			choice=$( echo "$choice" | awk -F: '{ print $1 }' )
+			
+			full_link=$( echo $video_url$( echo "$urls" | sed -n $choice\p ))
+			add_to_list "$list" "$title"' '$full_link
+			break
+		fi	
+		
+		## Stop other instances of mpv running
 		echo quit | socat - /tmp/mpvsocket	
 		notify-send "Playing '$choice'"
+
+		## This is bad, and it doesn't work half the time. Fix this TODO
 		echo "$choice" > /tmp/currently_playlist_pipecat
+
+		## Transform choice into choice index
 		choice=$( echo "$choice" | awk -F: '{ print $1 }' )
 
 		## Get the url with index $choice and play it in mpv
@@ -94,28 +148,47 @@ do
 		playlist=$( echo "" | dmenu -sb '#98005d' -l 0 -fn "Terminus:bold:size:15" -h 27   -p "Search for a playlist:"  )
 
 		search_playlist "$playlist"
-
+		
+		## Prompt user to select a playlist, then get the playlists's URL
 		choice=$( echo "$titles" | dmenu -sb '#98005d' -fn "Terminus:bold:size:15" -h 27 -l 10 )
 		choice=$( echo "$titles" | grep -n "$choice" | awk -F: '{ print $1 }' )
 		choice=$( echo "$urls" | sed -n $choice\p )
 			
+		## Get contents of playlist with url
 		get_playlist_content "$choice"
-
+		
+		## Prompt user to select a video/song from playlist
 		choice=$( echo "$titles" | dmenu -sb '#98005d' -fn "Terminus:bold:size:15" -h 27 -l 10 )
 
+		## EXTREMELY IMPORTANT! When option set to add to list, add to list and break out of the loop
+		if [[ -v add_to_list ]]
+		then
+			title="$choice"
+		        choice=$( echo "$choice" | awk -F: '{ print $1 }' )
+			full_link=$( echo $video_url$( echo "$urls" | sed -n $choice\p | awk -F"&list" '{ print $1 }' ))
+			add_to_list "$list" "$title"' '$full_link
+			break
+		fi
+
+		
 		if [[ $choice == "" ]]
 		then
 			break
 		fi
 		
+		## Stop other instances of mpv running
 		echo quit | socat - /tmp/mpvsocket
 		notify-send "Playing '$choice'"
-		echo "$choice" > /tmp/currently_playlist_pipecat		
 
-		choice=$( echo "$choice" | awk -F: '{ print $1 }' ) 
-		## Get the url with index $choice and play it in mpv
+		## Again, horrible TODO
+		echo "$choice" > /tmp/currently_playlist_pipecat		
 		
-		mpv $audio_mode --input-ipc-server=$socket_path "$video_url$( echo "$urls" | sed -n $choice\p )"
+		## Transform choice into choice index
+		choice=$( echo "$choice" | awk -F: '{ print $1 }' ) 
+
+		## Throw contents of playlist into a file, then play this playlist starting from index in mpv
+		echo "$urls" | sed "s/^/$sed_url/" | awk -F"&list" '{ print $1 }' > /tmp/pipecat_list
+		mpv $audio_mode --input-ipc-server=$socket_path -playlist=/tmp/pipecat_list --playlist-start=$((choice - 1))
 		
 	elif [[ $search_option == "Channel" ]]
 	then
@@ -123,34 +196,54 @@ do
 		channel=$( echo "" | dmenu -sb '#98005d' -l 0 -fn "Terminus:bold:size:15" -h 27   -p "Search for a channel:"  )
 
 		search_channel "$channel"	
-
+		
+		## Prompt user to select a channel, then get url of this channel's uploads playlist
 		choice=$( echo "$titles" | dmenu -sb '#98005d' -fn "Terminus:bold:size:15" -h 27 -l 10 )
 		choice=$( echo "$titles" | grep -n "$choice" | awk -F: '{ print $1 }' )
 		choice=$( echo "$urls" | sed -n $choice\p )
 		choice=$( echo "/playlist?list=""$choice" )
 		
-		echo "$urls"
-
 		get_playlist_content "$choice" 
+
+		## Make urls not stupid, 'cause for some god forsaken reason they don't want to work if you don't do this
+		urls=$( echo "$urls" | awk -F"&list" '{ print $1 }' )
+
+		## Prompt user to choose a video from a channel
 		choice=$( echo "$titles" | dmenu -sb '#98005d' -fn "Terminus:bold:size:15" -h 27 -l 10 )
-		echo $choice
+
+		## EXTREMELY IMPORTANT! When option set to add to list, add to list and break out of the loop
+		if [[ -v add_to_list ]]
+		then
+			title="$choice"
+			choice=$( echo "$choice" | awk -F: '{ print $1 }' )
+			full_link=$( echo $video_url$( echo "$urls" | sed -n $choice\p ))
+			add_to_list "$list" "$title"' '$full_link
+			break
+		fi
+
 		if [[ $choice == "" ]]
 		then
 			break
 		fi
 		
+		## Stop other mpv instances from running	
 		echo quit | socat - /tmp/mpvsocket
 		notify-send "Playing '$choice'"
+
+		## Terrible, TODO, let's move on
 		echo "$choice" > /tmp/currently_playlist_pipecat
 
+		## Transform choice into choice index
 		choice=$( echo "$choice" | awk -F: '{ print $1 }' )
 		
-		echo "$choice  <---------"
 		## Get the url with index $choice and play it in mpv
 		mpv $audio_mode --input-ipc-server=$socket_path "$video_url$( echo "$urls" | sed -n $choice\p )"
 	elif [[ $search_option == "Controlls" ]]
 	then
+		## Bad, bad, bad, bad
 		currently_playing=$( cat /tmp/currently_playlist_pipecat )
+
+		## Prompt user to choose an action
 		choice=$( echo -e "<<\n||\n>>\nX" | dmenu -sb '#98005d' -l 0 -fn "Terminus:bold:size:15" -h 27 -p "$currently_playing" ) 
 		case $choice in
 			"<<")
@@ -180,9 +273,18 @@ do
 		list=$(cat "$HOME""/.pipecat_turbo_lists" | grep "####- START LIST" | awk -F\< '{ print $2 }' | awk -F\> '{ print $1 }' | dmenu -sb '#98005d' -fn "Terminus:bold:size:15" -h 27 -l 10 )
 		
 		## Get the starting index and the file lenght of playlist file to get playlist starting and ending index in the next for loop. This should be done better than I did it but I'm so tired I just can't do it
+		echo $list
 		start_index=$( cat "$HOME""/.pipecat_turbo_lists" | grep -n "####- START LIST <$list>" | awk -F\: '{ print $1 }' )
 		file_lenght=$( cat  "$HOME""/.pipecat_turbo_lists" | wc -l )
 		
+		## If for some reason there's no starting index, break
+		if [[ -z $start_index ]]
+		then
+			echo "doesn't exist"
+			break
+		fi
+		
+		## This for loop checks for the nearest END LIST block
 		for index in $( seq $file_lenght)
 		do
 			line=$( echo "$file" | sed -n $index\p )
@@ -194,9 +296,13 @@ do
 		
 		## Extracting the urls between start_index and index - it should be named end_index or something but i juwst don't care anymore. This took 3 hours to implement. Never code while tired.
 		content=$(echo "$file" | sed -n "$((start_index + 1)),$((index - 1))""p" )
+
+		## Placing these urls into a /tmp file
 		echo "$content" | awk '{ print $NF }' > /tmp/pipecat_list
 
+		## Play through a playlist
 		mpv $audio_mode --input-ipc-server=$socket_path --playlist=/tmp/pipecat_list
+			
 	fi
 	break
 done
